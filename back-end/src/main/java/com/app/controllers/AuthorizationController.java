dbb934fb45aa434f99f8b8273b72fb8b6de26426
unknown <gworkout@ukr.net>
2019-11-13 08:13:53 -0800

    1st commit

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -1,5 +1,30 @@
 package com.app.controllers;
 
+import com.app.entities.User;
+import com.app.services.AuthorizationService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/test")
 public class AuthorizationController {
 
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @PostMapping("/register")
+    public String registration(@Valid @RequestBody User user,){
+
+        authorizationService.userRegister(user);
+        UserRecord userRecord = FirebaseAuth.getInstance();
+
+        System.out.println("Successfully created new user: " + userRecord.getUid());
+
+        return new Gson().toJson(user);
+    }
 }
--- back-end/src/main/java/com/app/services/AuthorizationService.java
+++ back-end/src/main/java/com/app/services/AuthorizationService.java
@@ -1,4 +1,17 @@
 package com.app.services;
 
+import com.app.entities.User;
+import com.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorizationService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void userRegister(User newUser){
+        userRepository.save(newUser);
+    }
 }
