ad9795306ef2101ce50065e9a1f62def47c5e142
Alena <alana.eremina@gmail.com>
2019-11-14 19:33:13 +0200

    31_authtoken: update controller

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -1,5 +1,48 @@
 package com.app.controllers;
 
+import com.app.configtoken.JwtTokenProvider;
+import com.app.services.UserServiceImpl;
+import com.app.validators.LoginRequest;
+import com.app.validators.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.app.configtoken.Constants.TOKEN_PREFIX;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/users")
 public class AuthorizationController {
 
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private UserValidator userValidator;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.provideToken(authentication);
+
+        //new JWTLoginSuccessResponse(true, jwt)
+        return ResponseEntity.ok(200);
+    }
 }
