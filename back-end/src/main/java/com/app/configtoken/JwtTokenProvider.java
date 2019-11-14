980acbc8ce349fdfe2365717f6430a2dc88026f9
Alena <alana.eremina@gmail.com>
2019-11-14 15:59:44 +0200

    31_authtoken: JwtTokenProvider for auth created

---
+++ back-end/src/main/java/com/app/configtoken/JwtTokenProvider.java
@@ -0,0 +1,40 @@
+package com.app.configtoken;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.app.configtoken.Constants.EXPIRATION_TIME;
+import static com.app.configtoken.Constants.SECRET;
+
+public class JwtTokenProvider {
+
+    public String provideToken(Authentication authentication){
+        User user = (User)authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime()+EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+
+}
