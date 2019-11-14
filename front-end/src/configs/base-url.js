38f7a0d269e290d656f1ce320e335f2280df26a1
dimavasilenko <dima.vasilenko96@gmail.com>
2019-11-14 17:29:15 +0200

    changed basicUrl

--- front-end/src/configs/base-url.js
+++ front-end/src/configs/base-url.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const setBaseURL = () => {
-  axios.defaults.baseURL = 'http://10.11.1.155:8081/';
+  axios.defaults.baseURL = 'https://grampus.herokuapp.com/';
   // axios.defaults.baseURL = 'http://localhost:8080';
 };
 
