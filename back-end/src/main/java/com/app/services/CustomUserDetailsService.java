cca164c65b39e5c784951fbf31c1ab04623976ba
e.eremina <e.eremina@avystele.com>
2019-11-15 13:35:23 +0200

    #31_auth_token:add CustomUserDetailService

---
+++ back-end/src/main/java/com/app/services/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.app.services;
+
+import com.app.entities.User;
+import com.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+   private UserRepository userRepository;
+
+   @Autowired
+   public CustomUserDetailsService(UserRepository userRepository) {
+      this.userRepository = userRepository;
+   }
+
+   @Override
+   public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+      return null;
+   }
+
+   @Transactional
+   public User loadUserById(Long id) {
+      User user = userRepository.getById(id);
+      if (user == null) throw new UsernameNotFoundException("User not found");
+      return user;
+   }
+}
