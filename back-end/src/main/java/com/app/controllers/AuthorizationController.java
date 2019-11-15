630a3af21488dd204b94ca38d8a4aec820a8633e
e.eremina <e.eremina@avystele.com>
2019-11-16 00:47:31 +0200

    #31_auth_token:AuthorithationController optimize import

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -2,14 +2,12 @@ package com.app.controllers;
 
 
 import com.app.configtoken.JwtTokenProvider;
-import com.app.entities.User;
 import com.app.services.UserService;
 import com.app.validators.JWTLoginSuccessResponse;
 import com.app.validators.LoginRequest;
 import com.app.validators.UserValidator;
 import com.app.validators.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
