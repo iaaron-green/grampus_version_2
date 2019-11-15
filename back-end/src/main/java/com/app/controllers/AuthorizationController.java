2a35ebf9a39afb8cee8577adb34f9e2d71097272
e.eremina <e.eremina@avystele.com>
2019-11-16 00:39:40 +0200

    #31_auth_token:AuthorizationController implemented

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -1,5 +1,64 @@
 package com.app.controllers;
 
+
+import com.app.configtoken.JwtTokenProvider;
+import com.app.entities.User;
+import com.app.services.UserService;
+import com.app.validators.JWTLoginSuccessResponse;
+import com.app.validators.LoginRequest;
+import com.app.validators.UserValidator;
+import com.app.validators.ValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.app.configtoken.Constants.TOKEN_PREFIX;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/users")
 public class AuthorizationController {
 
+   private ValidationErrorService validationErrorService;
+   private UserService userService;
+   private UserValidator userValidator;
+   private JwtTokenProvider tokenProvider;
+   private AuthenticationManager authenticationManager;
+
+   @Autowired
+   public AuthorizationController(ValidationErrorService validationErrorService, UserService userService,
+                         UserValidator userValidator,
+                         JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+      this.validationErrorService = validationErrorService;
+      this.userService = userService;
+      this.userValidator = userValidator;
+      this.tokenProvider = tokenProvider;
+      this.authenticationManager = authenticationManager;
+   }
+
+   @PostMapping("/login")
+   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+      ResponseEntity<?> errorMap = validationErrorService.mapValidationService(result);
+      if(errorMap != null) return errorMap;
+
+      Authentication authentication = authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                      loginRequest.getUsername(),
+                      loginRequest.getPassword()
+              )
+      );
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      String jwt = TOKEN_PREFIX +  tokenProvider.provideToken(authentication);
+
+      return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+   }
 }
