660a5fd4443fae9f5b741c46fc234780dca9ce53
e.eremina <e.eremina@avystele.com>
2019-11-16 00:38:47 +0200

    #31_auth_token:UserRepository update

--- back-end/src/main/java/com/app/repository/UserRepository.java
+++ back-end/src/main/java/com/app/repository/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+   User findByUsername(String login);
+
+   User getById(Long id);
 }
