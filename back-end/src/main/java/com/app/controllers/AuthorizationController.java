e3c90c04c52786ca1ae44cd5d8ce2d5b0aab18ec
e.eremina <e.eremina@avystele.com>
2019-11-18 00:33:33 +0200

    #31_auth_token:AuthorizationController registerUser

--- back-end/src/main/java/com/app/controllers/AuthorizationController.java
+++ back-end/src/main/java/com/app/controllers/AuthorizationController.java
@@ -2,12 +2,16 @@ package com.app.controllers;
 
 
 import com.app.configtoken.JwtTokenProvider;
+import com.app.entities.Profile;
+import com.app.entities.User;
+import com.app.services.ProfileService;
 import com.app.services.UserService;
 import com.app.validators.JWTLoginSuccessResponse;
 import com.app.validators.LoginRequest;
 import com.app.validators.UserValidator;
 import com.app.validators.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,16 +34,18 @@ public class AuthorizationController {
    private UserValidator userValidator;
    private JwtTokenProvider tokenProvider;
    private AuthenticationManager authenticationManager;
+   private ProfileService profileService;
 
    @Autowired
    public AuthorizationController(ValidationErrorService validationErrorService, UserService userService,
                          UserValidator userValidator,
-                         JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+                         JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager, ProfileService profileService) {
       this.validationErrorService = validationErrorService;
       this.userService = userService;
       this.userValidator = userValidator;
       this.tokenProvider = tokenProvider;
       this.authenticationManager = authenticationManager;
+      this.profileService = profileService;
    }
 
    @PostMapping("/login")
@@ -58,5 +64,17 @@ public class AuthorizationController {
       String jwt = TOKEN_PREFIX +  tokenProvider.provideToken(authentication);
 
       return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+   }
+
+   @PostMapping("/register")
+   public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
+      userValidator.validate(user,result);
+
+      ResponseEntity<?> errorMap = validationErrorService.mapValidationService(result);
+      if(errorMap != null)return errorMap;
+
+      User newUser = userService.saveUser(user);
+      Profile newProfile = profileService.saveProfile(new Profile(newUser));
+      return  new ResponseEntity<>(newUser, HttpStatus.CREATED);
    }
 }
