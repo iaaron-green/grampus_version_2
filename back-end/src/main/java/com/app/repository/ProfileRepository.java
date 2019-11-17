9add0c16ffcd1e4f76bc3c2dbca4761c3d97a694
e.eremina <e.eremina@avystele.com>
2019-11-18 00:35:19 +0200

    #31_auth_token:ProfileRepository

---
+++ back-end/src/main/java/com/app/repository/ProfileRepository.java
@@ -0,0 +1,15 @@
+package com.app.repository;
+
+import com.app.entities.Profile;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends CrudRepository<Profile, Long> {
+   Optional<Profile> findById(Long profileId);
+
+   @Override
+   Iterable<Profile> findAll();
+}
