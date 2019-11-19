7e7210e75bab95458b1ed8d0b721fa9b395bdff6
dimavasilenko <dima.vasilenko96@gmail.com>
2019-11-19 12:58:45 +0200

    upload photo in profile and changed url

--- front-end/src/components/Dashboard/Profile/Person/Person.js
+++ front-end/src/components/Dashboard/Profile/Person/Person.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
+
 import styles from './Person.module.css';
 
-const Person = ({ profile, user }) => {
+const Person = ({profile, user}) => {
   return (
     <div className={styles.person}>
-      <div className={styles.imgBG}>
-        <img
-          className={styles.img}
-          src={profile.profilePicture}
-          alt={profile.id}
-          width="150px"
-          height="100px"
-        />
-      </div>
+      <img
+        className={styles.img}
+        src={profile.profilePicture}
+        alt={profile.id}
+        width="180px"
+        height="110px"
+      />
+      <input type='file' />
       <div className={styles.userName}>{user.fullName}</div>
       <div className={styles.jobTitle}>{user.jobTitle}</div>
       <div className={styles.likes}>{user.likes}</div>
@@ -23,3 +23,18 @@ const Person = ({ profile, user }) => {
 };
 
 export default Person;
+
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+      const img = document.querySelector('img');  // $('img')[0]
+      img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      img.onload = imageIsLoaded;
+    }
+  });
+});
+
+function imageIsLoaded() {
+  // alert(this.src);  // blob url
+  // update width and height ...
+}
