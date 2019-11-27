ff0baf6c465d69a928741026aae112fd6881ed06
oleg <oshchekin@gmail.com>
2019-11-27 17:51:56 +0200

    6.3:fix findAllRatingById

--- back-end/src/main/java/com/app/repository/RatingRepository.java
+++ back-end/src/main/java/com/app/repository/RatingRepository.java
@@ -11,11 +11,9 @@ import java.util.List;
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
     @Query(
-            value = "SELECT id, profile_id, profile_id as user_id, rating_type, COUNT(rating_type) as raiting_count FROM test_db.ratings GROUP BY profile_id, rating_type",
+            value = "SELECT id, profile_id, rating_source_username,profile_id as user_id, rating_type, COUNT(rating_type) as raiting_count FROM grampus_db.ratings GROUP BY profile_id, rating_type",
             nativeQuery = true)
     List<Rating> findAllRatingById();
-
-    Long countRatingTypeById(Long id);
 
     @Query(
             value = "SELECT id, profile_id, profile_id as user_id, rating_type, COUNT(rating_type) as raiting_count FROM grampus_db.ratings GROUP BY rating_type",
@@ -23,7 +21,7 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> groupAndCountRatingTypeById();
 
     @Query(
-            value = "SELECT COUNT(rating_type) FROM test_db.ratings WHERE profile_id = ? AND rating_type = ?",
+            value = "SELECT COUNT(rating_type) FROM grampus_db.ratings WHERE profile_id = ? AND rating_type = ?",
             nativeQuery = true)
     Long countRatingType(Long id, String ratingType);
 
@@ -34,7 +32,7 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> getAllByRatingType(String s);
 
     @Query(
-            value = "SELECT rating_type FROM test_db.ratings WHERE rating_source_username = ? AND profile_id = ?",
+            value = "SELECT rating_type FROM grampus_db.ratings WHERE rating_source_username = ? AND profile_id = ?",
             nativeQuery = true)
     List<String> getProfileRatingTypes (String username, Long profileId);
 
