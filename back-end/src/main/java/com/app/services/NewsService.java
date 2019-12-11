267a3538a816ef83861d6f8c9118c2701fd3f9f9
Alena <alana.eremina@gmail.com>
2019-12-11 16:12:50 +0200

    3_news: implements get all news

--- back-end/src/main/java/com/app/services/NewsService.java
+++ back-end/src/main/java/com/app/services/NewsService.java
@@ -2,6 +2,7 @@ package com.app.services;
 
 import com.app.DTO.DTONews;
 import com.app.exceptions.CustomException;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +14,9 @@ public interface NewsService {
 
     void saveDTONews(String title, String content, MultipartFile file, Principal principal) throws CustomException;
 
-    List<DTONews> getAllNews(Principal principal, Integer page, Integer size) throws CustomException;
+   // List<DTONews> getAllNews(Principal principal, Integer page, Integer size) throws CustomException;
 
     void saveComment(Long id, String comment, Principal principal) throws CustomException;
+
+    Page<DTONews> getAllNews(Principal principal, Integer page, Integer size);
 }
