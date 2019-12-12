a153ce3b5d297796899903f1a67dafca563efce4
oleg <oshchekin@gmail.com>
2019-12-12 10:35:30 +0200

    refactor profile options

--- back-end/src/main/java/com/app/Application.java
+++ back-end/src/main/java/com/app/Application.java
@@ -3,10 +3,17 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:config/test/config.properties")
+//        @PropertySource("file:${config.path}/dev-config.properties")
+})
 @EnableJms
 public class Application {
     @Bean
