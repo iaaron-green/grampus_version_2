078ef723cdbde19df3d7a9de8cdc740da4c7eeb4
Vadym <gworkout@ukr.net>
2020-01-08 17:12:09 +0200

    work version private chat

--- back-end/src/main/java/com/app/Application.java
+++ back-end/src/main/java/com/app/Application.java
@@ -1,6 +1,5 @@
 package com.app;
 
-import com.app.netty.ChatServer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
